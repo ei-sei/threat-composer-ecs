568e87af916ac90b4c84a5ae5389baeb8a9437fe
ei-sei <sk.ahm2019@gmail.com>
2026-05-17 19:09:49 +0100

    add: terraform alb - redirects to https

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,118 @@ module "ecr" {
 }
 
 module "acm" {
-  source      = "./modules/acm"
-  domain_name = var.domain_name
-  environment = var.environment
+  source             = "./modules/acm"
+  domain_name        = var.domain_name
+  environment        = var.environment
   cloudflare_zone_id = var.cloudflare_zone_id
 }
 
 
+## ALB:
 
+resource "aws_security_group" "alb_sg" {
+  name        = "tm-alb-sg"
+  description = "Security group for the ALB"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "tm-alb-sg"
+    environment = var.environment
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "tm-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets = module.network.public_subnet_ids
+
+
+  tags = {
+    Name        = "tm-alb"
+    environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tm-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.network.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
+  tags = {
+    Name        = "tm-tg"
+    environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = {
+    Name        = "tm-alb-listener-http"
+    environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = module.acm.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  tags = {
+    Name        = "tm-alb-listener-https"
+    environment = var.environment
+  }
+}
